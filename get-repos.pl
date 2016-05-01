23556aa7af5a7be432e1460524a70bf08e1c4a3e
JJ Merelo <jjmerelo@gmail.com>
2016-05-01 23:14:22 +0200

    Adds local networks

--- get-repos.pl
+++ get-repos.pl
@@ -11,7 +11,7 @@ my $gh = Net::GitHub->new(
         access_token => $ENV{'GH_TOKEN'} # from above
 			     );
 
-my $place = shift || "Granada";
+my $place = shift || "Almería";
 my $dir = shift || "../top-github-users-data/data";
 
 my @data = read_lines( "$dir/user-data-$place.csv" );
@@ -19,7 +19,41 @@ my @data = read_lines( "$dir/user-data-$place.csv" );
 die "No data" if !@data;
 
 shift @data;
+my $repos = $gh->repos;
+my %all_repos;
 for my $d (@data ) {
   my ($username,@foo) = split(/;\s*/,$d);
-  my @repos =$gh->repos->list_user($username);
+  my @repos =$repos->list_user($username);
+  while ( $repos->has_next_page ) {
+    push @repos, $repos->next_page;
+  }
+  for my $r ( @repos ) {
+    if ( !$r->{'fork'} & !$all_repos{$r->{'name'}}) {
+      $repos->set_default_user_repo( $username, $r->{'name'});
+      my @contributors;
+      if ( ref $repos->contributors eq 'ARRAY' ) {
+	@contributors =  map( $_->{'login'}, @{$repos->contributors});
+      } else {
+	push @contributors, $username;
+      }
+      $all_repos{$r->{'name'}} = \@contributors;
+    }
+  }
 }
+
+#Download collaborators
+my %relations;
+for my $r (keys %all_repos ) {
+  my @collaborators =  @{$all_repos{$r}} ;
+  for ( my $i = 0; $i < $#collaborators; $i++ ) {
+    for (my $j = $i+1; $j <= $#collaborators; $j ++ ) {
+      $relations{"$collaborators[$i];$collaborators[$j]"}++;
+    }
+  }
+}
+
+for my $i ( sort { $relations{$a} <=> $relations{$b} } keys %relations ) {
+  for ( my $j = 0; $j < $relations{$i};$j ++ ) {
+    say $i;
+  }
+}    
